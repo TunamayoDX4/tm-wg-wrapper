a42811f72f22607724602a62ae62e9333a18cb5c
TunamayoDX4 <tsunamayodx4@outlook.jp>
2023-04-15 21:09:21 +0900

    preludeの追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,30 @@
 pub mod ctx;
 pub mod util;
+
+pub mod prelude {
+    pub use winit::{
+        self, 
+        window::Window, 
+        event::{
+            ElementState, 
+            VirtualKeyCode, 
+            MouseButton, 
+            MouseScrollDelta, 
+        }, 
+    };
+    pub use wgpu;
+    pub use wgpu_glyph;
+    pub use rodio;
+
+    pub use crate::ctx::{
+        Context, 
+        gfx::{
+            GfxCtx, 
+            Renderer, 
+        }, 
+        sfx::{
+            SfxCtx, 
+        }, 
+    };
+    pub use crate::util::*;
+}
