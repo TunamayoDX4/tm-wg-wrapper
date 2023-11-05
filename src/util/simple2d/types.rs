784d693d17e2604a5e7cd75f131436d4b4d442af
TunamayoDX4 <tsunamayodx4@outlook.jp>
2023-11-05 15:32:17 +0900

    テクスチャへの関数の追加

--- src/util/simple2d/types.rs
+++ src/util/simple2d/types.rs
@@ -1,5 +1,7 @@
 use std::io::Read;
 
+use image::{ImageBuffer, Rgba};
+
 /// 描画が行われる領域
 pub struct VisibleField {
     cam_pos: nalgebra::Point2<f32>, 
@@ -69,36 +71,29 @@ impl Camera {
 pub struct Texture {
     pub bind_group: wgpu::BindGroup, 
     pub texture_size: nalgebra::Vector2<f32>, 
+    pub sampler: wgpu::Sampler, 
 }
 impl Texture {
-    pub fn new(
+    pub fn create_bind_group<C: std::ops::Deref<Target = [u8]>>(
         gfx: &crate::ctx::gfx::GfxCtx, 
         bind_group_layout: &wgpu::BindGroupLayout, 
-        path: impl AsRef<std::path::Path>, 
-    ) -> Result<Self, Box<dyn std::error::Error>> {
-        // 画像の読み込み・インスタンス生成
-        let diffuse_rgba8 = {
-            let mut file = std::fs::File::open(path)?;
-            let mut diffuse_bytes = Vec::new();
-            file.read_to_end(&mut diffuse_bytes)?;
-            image::load_from_memory(&diffuse_bytes)?
-                .to_rgba8()
-        };
-
+        sampler: &wgpu::Sampler, 
+        image: ImageBuffer<Rgba<u8>, C>, 
+    ) -> ((u32, u32), wgpu::BindGroup) {
         // 画像の大きさ情報の取得
-        let dimensions = diffuse_rgba8.dimensions();
+        let dim = image.dimensions();
 
         // GPUで画像を扱うための大きさの情報を初期化
-        let texture_size = wgpu::Extent3d {
-            width: dimensions.0, 
-            height: dimensions.1, 
+        let tex_size = wgpu::Extent3d {
+            width: dim.0, 
+            height: dim.1, 
             depth_or_array_layers: 1, 
         };
 
         // 拡散テクスチャのインスタンス生成
-        let diffuse_texture = gfx.device.create_texture(
+        let texture = gfx.device.create_texture(
             &wgpu::TextureDescriptor {
-                size: texture_size, 
+                size: tex_size, 
                 mip_level_count: 1, 
                 sample_count: 1, 
                 dimension: wgpu::TextureDimension::D2, 
@@ -112,27 +107,70 @@ impl Texture {
         // GPUのキューにテクスチャ情報を書き込む
         gfx.queue.write_texture(
             wgpu::ImageCopyTexture {
-                texture: &diffuse_texture, 
+                texture: &texture, 
                 mip_level: 0, 
                 origin: wgpu::Origin3d::ZERO, 
                 aspect: wgpu::TextureAspect::All, 
             }, 
-            &diffuse_rgba8, 
+            &image, 
             wgpu::ImageDataLayout { 
                 offset: 0, 
-                bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0), 
-                rows_per_image: std::num::NonZeroU32::new(dimensions.1) 
+                bytes_per_row: std::num::NonZeroU32::new(4 * dim.0), 
+                rows_per_image: std::num::NonZeroU32::new(dim.1) 
             }, 
-            texture_size
+            tex_size
         );
 
         // ビューの作成
-        let diffuse_texture_view = diffuse_texture.create_view(
+        let texture_view = texture.create_view(
             &wgpu::TextureViewDescriptor::default()
         );
 
+        // バインドグループの作成
+        (dim, gfx.device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                layout: bind_group_layout, 
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0, 
+                        resource: wgpu::BindingResource::TextureView(&texture_view), 
+                    }, 
+                    wgpu::BindGroupEntry {
+                        binding: 1, 
+                        resource: wgpu::BindingResource::Sampler(sampler), 
+                    }, 
+                ], 
+                label: Some("diffuse bind group"), 
+            }
+        ))
+    }
+
+    pub fn update_image<C: std::ops::Deref<Target = [u8]>>(
+        &mut self, 
+        gfx: &crate::ctx::gfx::GfxCtx, 
+        bind_group_layout: &wgpu::BindGroupLayout, 
+        image: ImageBuffer<Rgba<u8>, C>, 
+    ) -> wgpu::BindGroup {
+        let (dim, bind_group) = Self::create_bind_group(
+            gfx, 
+            bind_group_layout, 
+            &self.sampler, 
+            image
+        );
+
+        self.texture_size = [dim.0 as f32, dim.1 as f32].into();
+
+        // すり替え
+        std::mem::replace(&mut self.bind_group, bind_group)
+    }
+
+    pub fn from_image<C: std::ops::Deref<Target = [u8]>>(
+        gfx: &crate::ctx::gfx::GfxCtx, 
+        bind_group_layout: &wgpu::BindGroupLayout, 
+        image: ImageBuffer<Rgba<u8>, C>, 
+    ) -> Self {
         // サンプラの作成
-        let diffuse_sampler = gfx.device.create_sampler(&wgpu::SamplerDescriptor {
+        let sampler = gfx.device.create_sampler(&wgpu::SamplerDescriptor {
             address_mode_u: wgpu::AddressMode::ClampToEdge, 
             address_mode_v: wgpu::AddressMode::ClampToEdge, 
             address_mode_w: wgpu::AddressMode::ClampToEdge, 
@@ -142,27 +180,37 @@ impl Texture {
             ..Default::default()
         });
 
-        // バインドグループの作成
-        let diffuse_bind_group = gfx.device.create_bind_group(
-            &wgpu::BindGroupDescriptor {
-                layout: bind_group_layout, 
-                entries: &[
-                    wgpu::BindGroupEntry {
-                        binding: 0, 
-                        resource: wgpu::BindingResource::TextureView(&diffuse_texture_view), 
-                    }, 
-                    wgpu::BindGroupEntry {
-                        binding: 1, 
-                        resource: wgpu::BindingResource::Sampler(&diffuse_sampler), 
-                    }, 
-                ], 
-                label: Some("diffuse bind group"), 
-            }
+        let (dim, bind_group) = Self::create_bind_group(
+            gfx, 
+            bind_group_layout, 
+            &sampler, 
+            image
         );
 
-        Ok(Self { 
-            bind_group: diffuse_bind_group, 
-            texture_size: [dimensions.0 as f32, dimensions.1 as f32].into(), 
-        })
+        Self { 
+            bind_group, 
+            texture_size: [dim.0 as f32, dim.1 as f32].into(), 
+            sampler, 
+        }
+    }
+
+    pub fn new(
+        gfx: &crate::ctx::gfx::GfxCtx, 
+        bind_group_layout: &wgpu::BindGroupLayout, 
+        path: impl AsRef<std::path::Path>, 
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        // 画像の読み込み・インスタンス生成
+
+        Ok(Self::from_image(
+            gfx, 
+            bind_group_layout, 
+            {
+                let mut file = std::fs::File::open(path)?;
+                let mut diffuse_bytes = Vec::new();
+                file.read_to_end(&mut diffuse_bytes)?;
+                image::load_from_memory(&diffuse_bytes)?
+                    .to_rgba8()
+            }
+        ))
     }
 }
