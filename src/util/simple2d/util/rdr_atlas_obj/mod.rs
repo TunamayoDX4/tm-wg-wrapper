b50e7dd2a5e1b4fe5597c838a1f39fad5bc37cac
TunamayoDX4 <tsunamayodx4@outlook.jp>
2023-11-09 15:23:07 +0900

    デバッグ用処理の挿入

--- src/util/simple2d/util/rdr_atlas_obj/mod.rs
+++ src/util/simple2d/util/rdr_atlas_obj/mod.rs
@@ -68,7 +68,7 @@ impl<K, I> AtlasRenderingModule<K, I> where
     ), error::RdrInitError<
         K, I, Ii
     >> where
-        Q: Eq + Hash + ?Sized + ToOwned<Owned = K> + 'a, 
+        Q: Eq + Hash + ?Sized + ToOwned<Owned = K> + std::fmt::Debug + 'a, 
         K: std::borrow::Borrow<Q>, 
         P: ?Sized + AsRef<std::path::Path> + 'a, 
         Ii: AtlasControllerInitializer<
@@ -199,7 +199,7 @@ impl<K, I> AtlasRenderer<K, I> where
     ), 
         error::RdrInitError<K, I, Ii>, 
     > where
-        Q: Eq + Hash + ?Sized + ToOwned<Owned = K> + 'a, 
+        Q: Eq + Hash + ?Sized + ToOwned<Owned = K> + std::fmt::Debug + 'a, 
         K: std::borrow::Borrow<Q>, 
         P: ?Sized + AsRef<std::path::Path> + 'a, 
         Ii: AtlasControllerInitializer<
